db4f9bbe789fdcf194f4f0ca01bf65f4ec790195
Dapiro_Denis <dapirodenis@gmail.com>
2024-10-07 22:51:27 +0300

    Мигарции

---
+++ Bookinist/Data/DbRegistator.cs
@@ -0,0 +1,35 @@
+﻿using Bookinist.Dal.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookinist.Data
+{
+    static class DbRegistator
+    {
+        public static IServiceCollection AddDataBase(this IServiceCollection services, IConfiguration configuration) => services
+            .AddDbContext<BookinistDb>(opt =>
+            {
+                var type = configuration["Type"];
+                switch (type)
+                {
+                    case null: throw new InvalidOperationException($"Не определён тип базы данных");
+                    default: throw new InvalidOperationException($"Тип подключения {type} не поддерживается");
+                    case "MSSQL":
+                        opt.UseSqlServer(configuration.GetConnectionString(type));
+                        break;
+                    case "MSLite":
+                        opt.UseSqlite(configuration.GetConnectionString(type));
+                            break;
+                    case "InMemory":
+                        opt.UseInMemoryDatabase("Bookinist.db");
+                        break;
+                }
+            });
+    }
+}
